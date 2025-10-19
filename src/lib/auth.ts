4597352fb084a3216c039460f79af5a08d477ff9
Bhushan Lagare <bhushanlagare7@gmail.com>
2025-10-20 00:58:35 +0530

    10: payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,31 @@
+import { checkout, polar, portal } from '@polar-sh/better-auth';
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import prisma from '@/lib/db';
+import { polarClient } from './polar';
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
   emailAndPassword: { enabled: true, autoSignIn: true },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: 'd330f1b8-693e-4716-aad1-f8ed551c5120',
+              slug: 'pro',
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
