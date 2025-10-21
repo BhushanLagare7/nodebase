9fd0896e6132e94a0aa49e762f96404ed361441f
Bhushan Lagare <bhushanlagare7@gmail.com>
2025-10-21 22:07:28 +0530

    12: workflows pagination

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import type { SearchParams } from 'nuqs/server';
 import { prefetchWorkflows } from '@/features/workflows/server/prefetch';
 import { requireAuth } from '@/lib/auth-utils';
 import { HydrateClient } from '@/trpc/server';
@@ -7,11 +8,18 @@ import {
   WorkflowsContainer,
   WorkflowsList,
 } from '@/features/workflows/components/workflows';
+import { workflowsParamsLoader } from '@/features/workflows/server/params-loader';
 
-const Workflows = async () => {
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
+
+const Workflows = async ({ searchParams }: Props) => {
   await requireAuth();
 
-  prefetchWorkflows();
+  const params = await workflowsParamsLoader(searchParams);
+
+  prefetchWorkflows(params);
 
   return (
     <WorkflowsContainer>
