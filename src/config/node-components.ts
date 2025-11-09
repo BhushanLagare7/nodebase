dab631aad9332123bf98ef1d0b6c2a12308afec3
Bhushan Lagare <bhushanlagare7@gmail.com>
2025-11-09 20:01:33 +0530

    23: stripe trigger

--- src/config/node-components.ts
+++ src/config/node-components.ts
@@ -6,6 +6,7 @@ import { InitialNode } from "@/components/initial-node";
 
 import { HttpRequestNode } from "@/features/executions/components/http-request/node";
 import { ManualTriggerNode } from "@/features/triggers/components/manual-trigger/node";
+import { StripeTriggerNode } from "@/features/triggers/components/stripe-trigger/node";
 import { GoogleFormTrigger } from "@/features/triggers/components/google-form-trigger/node";
 
 export const nodeComponents = {
@@ -13,6 +14,7 @@ export const nodeComponents = {
   [NodeType.HTTP_REQUEST]: HttpRequestNode,
   [NodeType.MANUAL_TRIGGER]: ManualTriggerNode,
   [NodeType.GOOGLE_FORM_TRIGGER]: GoogleFormTrigger,
+  [NodeType.STRIPE_TRIGGER]: StripeTriggerNode,
 } as const satisfies NodeTypes;
 
 export type RegisteredNodeTypes = keyof typeof nodeComponents;
